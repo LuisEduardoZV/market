3e16d4831d003a6de8cd3dbf783661d0e18dfbf7
Luis Z <lezv.dc@gmail.com>
2024-07-02 14:06:05 -0600

    Banner adjust

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const NAME_APP = 'WeatherApp'
 
 export const BASE_URL_API = 'https://dummyjson.com/'
 
-export const API_KEY_IMAGES = 'gJQr0DS7IS942x9ADrxMMegraoSKSlbrIcgFo4G6hzFMgOJ4JE1Dhepf'
+export const API_KEY_IMAGES = import.meta.env.VITE_API_KEY_IMAGES
 
 const config = {
   theme: 'light'
--- src/ui-components/Banner.jsx
+++ src/ui-components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
   return (
     <Flex style={{ width: '100%', overflowX: 'auto', position: 'relative' }}>
       {banner && banner.map((img, idx) => (<img key={idx} src={img?.url} alt={img?.alt} />))}
-      <Space direction='vertical' style={{ position: 'fixed', top: '40%', left: '50%', transform: 'translate(-50%,-50%)', width: '100%', textAlign: 'center', paddingBlock: 25, backgroundColor: 'rgba(255,255,255,0.2)', backdropFilter: 'blur(10px)' }}>
+      <Space direction='vertical' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', width: '100%', textAlign: 'center', paddingBlock: 25, backgroundColor: 'rgba(255,255,255,0.2)', backdropFilter: 'blur(10px)' }}>
         <Title level={1}>Market Store</Title>
         <Text strong>Ropa a la moda y de tendencia de grandes diseñadores</Text>
       </Space>
