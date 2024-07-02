9cddee84488938990af45e5c3063823a05b2a2ab
Luis Z <lezv.dc@gmail.com>
2024-07-02 12:17:12 -0600

    Query client provider ready to use

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,16 @@ import { ConfigProvider } from './context/ConfigContext.jsx'
 import { MAIN_PATH } from './config.js'
 import './index.css'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ConfigProvider>
-    <BrowserRouter basename={MAIN_PATH}>
-      <App />
-    </BrowserRouter>
-  </ConfigProvider>
+  <QueryClientProvider client={queryClient}>
+    <ConfigProvider>
+      <BrowserRouter basename={MAIN_PATH}>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
+  </QueryClientProvider>
 )
