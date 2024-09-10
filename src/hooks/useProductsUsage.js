9034d47d87317eb2978f541a8ad92ac9f9b5c437
Luis Z <lezv.dc@gmail.com>
2024-09-10 13:31:43 -0600

    Home animations

--- src/hooks/useProductsUsage.js
+++ src/hooks/useProductsUsage.js
@@ -3,17 +3,17 @@ import { useQuery } from 'react-query'
 import { getClothesProducts, getDisscountProducts, getTechProducts, getTopProducts } from '../services/productsFun'
 
 export function useProductsUsage () {
-  const { data: topProducts } = useQuery('topProducts', getTopProducts, { refetchOnWindowFocus: false })
+  const { data: topProducts, isLoading: topProductsLoading } = useQuery('topProducts', getTopProducts, { refetchOnWindowFocus: false })
 
-  const { data: disscountProducts } = useQuery('disscountProducts', getDisscountProducts, { refetchOnWindowFocus: false })
+  const { data: disscountProducts, isLoading: disscountProductsLoading } = useQuery('disscountProducts', getDisscountProducts, { refetchOnWindowFocus: false })
 
-  const { data: topClothes } = useQuery('topClothes', getClothesProducts, {
+  const { data: topClothes, isLoading: topClothesLoading } = useQuery('topClothes', getClothesProducts, {
     refetchOnWindowFocus: false
   })
 
-  const { data: techProd } = useQuery('techProd', getTechProducts, {
+  const { data: techProd, isLoading: techProdLoading } = useQuery('techProd', getTechProducts, {
     refetchOnWindowFocus: false
   })
 
-  return { disscountProducts, topProducts, topClothes, techProd }
+  return { disscountProducts, topProducts, topClothes, techProd, loading: topProductsLoading || disscountProductsLoading || topClothesLoading || techProdLoading }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useEffect, useRef } from 'react'
+
 import { Divider, Flex, Typography } from 'antd'
 
 import { useProductsUsage } from '../hooks/useProductsUsage'
@@ -9,38 +14,100 @@ import ShoesByGender from './components/ShoesByGender'
 
 const { Text, Title } = Typography
 
+gsap.registerPlugin(useGSAP, ScrollTrigger)
+
+ScrollTrigger.config({
+  autoRefreshEvents: 'DOMContentLoaded,load,resize'
+})
+
 const Home = () => {
-  const { topProducts, topClothes, techProd } = useProductsUsage()
+  const { topProducts, topClothes, techProd, loading } = useProductsUsage()
+  const container = useRef()
+
+  useGSAP(() => {
+    const sections = gsap.utils.toArray('.showAsApear')
+    sections.forEach((element, i) => {
+      gsap.from(element, {
+        opacity: 0,
+        scrollTrigger: {
+          trigger: element,
+          start: 'top 80%',
+          end: 'bottom 40%',
+          onEnter: () => {
+            gsap.to(element, {
+              duration: 1,
+              opacity: 1,
+              x: 0
+            })
+          },
+          onLeave: () => {
+            gsap.to(element, {
+              duration: 1,
+              opacity: 0
+            })
+          },
+          onEnterBack: () => {
+            gsap.to(element, {
+              duration: 1,
+              opacity: 1,
+              x: 0
+            })
+          },
+          onLeaveBack: () => {
+            gsap.to(element, {
+              duration: 1,
+              opacity: 0,
+              x: i % 2 === 0 && i !== 0 ? '100%' : 0
+            })
+          }
+        }
+      })
+    })
+  }, {
+    scope: container
+  })
+
+  useEffect(() => {
+    if (!loading) {
+      gsap.delayedCall(0.5, () => {
+        ScrollTrigger.refresh()
+      })
+    }
+  }, [loading])
 
   return (
-    <Flex vertical>
+    <Flex vertical ref={container} style={{ overflowX: 'hidden' }}>
       <Flex style={{ width: '100%', overflow: 'hidden' }} vertical>
         <Title level={3} style={{ marginBlock: '3%', paddingLeft: '2%' }}>People's favourite products!</Title>
         <CarouselFavProd products={topProducts} />
       </Flex>
 
-      <PromoBanner />
+      <Flex className='showAsApear'>
+        <PromoBanner />
+      </Flex>
 
-      <ShoesByGender />
+      <div className='showAsApear'>
+        <ShoesByGender />
+      </div>
 
-      <Flex style={{ width: '100%', overflow: 'hidden' }} vertical>
+      <Flex vertical className='showAsApear' style={{ width: '100%', overflowX: 'hidden' }}>
         <Title level={3} style={{ marginBlock: '3%', paddingLeft: '2%' }}>Fashion to your taste!</Title>
         <CarouselProducts products={topClothes} typeCarousel='discProd' />
       </Flex>
 
-      <Flex style={{ width: '100%', overflow: 'hidden' }} vertical>
+      <Flex style={{ width: '100%', overflow: 'hidden' }} vertical className='showAsApear'>
         <Title level={3} style={{ marginBlock: '3%', paddingLeft: '2%' }}>Technology products</Title>
         <CarouselProducts products={techProd} typeCarousel='techProd' />
       </Flex>
 
-      <Flex vertical>
+      <Flex vertical className='showAsApear' style={{ overflow: 'hidden' }}>
         <Title level={3} style={{ marginBlock: '3%', paddingLeft: '2%' }}>You may also be interested in</Title>
         <PortraitBanner />
       </Flex>
 
       <Divider style={{ marginBlock: '5%' }} />
 
-      <Flex vertical style={{ maxWidth: '45%', width: '100%', alignItems: 'center', alignSelf: 'center', textAlign: 'center', gap: 10, paddingBottom: '2%' }}>
+      <Flex vertical style={{ maxWidth: '45%', width: '100%', alignItems: 'center', alignSelf: 'center', textAlign: 'center', gap: 10, paddingBottom: '2%' }} className='showAsApear'>
         <Title level={2} style={{ fontWeight: 400 }}>Get a 5% discount</Title>
         <Text type='secondary' italic style={{ fontSize: '1.1rem' }}>Subscribe to our newsletter and get a 5% discount on your next purchase and exclusive promotions.</Text>
         <Flex className='button-borders' style={{ marginTop: 20 }}>
