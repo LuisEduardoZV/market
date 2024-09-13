382a790bcaf750b4ceb77a456e8477d655316e43
Luis Z <lezv.dc@gmail.com>
2024-09-13 11:27:18 -0600

    Estilos en jsx de Product a css

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -81,29 +81,29 @@ const Product = ({ hasDiscount }) => {
 
   if (isLoading || loadingRelated) return null
   return (
-    <Flex vertical style={{ minHeight: '100vh', height: '100%', paddingTop: '4%', paddingInline: '2%' }}>
-      <Flex style={{ width: '100%', justifyContent: 'space-between', position: 'relative', height: 'max-content', minHeight: '75vh' }}>
-        <Flex style={{ maxWidth: '30%', width: '100%', position: 'sticky', top: '10%', height: 'fit-content' }}>
+    <Flex vertical className='product-page'>
+      <Flex className='product-info-container'>
+        <Flex className='product-image-container'>
           <Space direction='vertical'>
             {data && data?.images && data.images.map((item, idx) => (
-              <Flex key={idx} style={{ maxHeight: 50, maxWidth: 50, cursor: 'pointer', borderWidth: 1, borderStyle: 'solid', borderColor: token.colorBgBase }} onMouseEnter={() => ref.current.goTo(idx)}>
-                <img src={item} alt={'Imge preview ' + idx} style={{ width: 50, objectFit: 'contain', height: 50 }} />
+              <Flex key={idx} className='product-image-preview' onMouseEnter={() => ref.current.goTo(idx)}>
+                <img src={item} alt={'Imge preview ' + idx} />
               </Flex>
             ))}
           </Space>
           <Flex vertical style={{ width: '100%', maxWidth: '100%' }}>
             <Carousel ref={ref} arrows autoplay infinite={false} style={{ width: '100%' }}>
               {data && data?.images && data.images.map((item, idx) => (
-                <Flex key={idx} style={{ maxHeight: 500 }}>
-                  <img src={item} alt={'Imge preview ' + idx} style={{ width: 'auto', objectFit: 'contain', height: 500 }} />
+                <Flex key={idx} className='product-image-carousel'>
+                  <img src={item} alt={'Imge preview ' + idx} />
                 </Flex>
               ))}
             </Carousel>
           </Flex>
         </Flex>
-        <Flex vertical style={{ maxWidth: '40%', width: '100%' }}>
-          <Title level={2} style={{ margin: 0 }}>{data.title}</Title>
-          <Title level={5} style={{ margin: 0, color: token.colorPrimaryText, fontStyle: 'italic', fontWeight: 600 }}>{data.brand}</Title>
+        <Flex vertical className='product-info'>
+          <Title level={2}>{data.title}</Title>
+          <Title level={5}>{data.brand}</Title>
           <Flex style={{ alignItems: 'center' }}>
             <Text type='secondary' style={{ fontSize: '0.8rem' }}>{data.rating}</Text>
             <Divider type='vertical' />
@@ -120,25 +120,25 @@ const Product = ({ hasDiscount }) => {
               </Fragment>
             ))}
           </Flex>
-          <Flex vertical style={{ marginTop: 50, gap: 50 }}>
+          <Flex vertical className='info-dimensions-product'>
             <Title level={3}>Package dimensions</Title>
-            <Flex style={{ alignItems: 'center', width: 'auto', justifyContent: 'center', position: 'relative' }}>
-              <IconBoxDimension color={token.colorPrimaryText} lineColor={token.colorPrimaryBgHover} style={{ width: '80%' }} />
-              <Text italic style={{ position: 'absolute', top: '40%', left: '80%', transform: 'translate(-50%, -50%)', fontWeight: 400 }}>
+            <Flex>
+              <IconBoxDimension color={token.colorPrimaryText} lineColor={token.colorPrimaryBgHover} />
+              <Text italic>
                 {data.dimensions.height} in
               </Text>
-              <Text italic style={{ position: 'absolute', top: '80%', left: '70%', transform: 'translate(-50%, -50%)', fontWeight: 400 }}>
+              <Text italic>
                 {data.dimensions.depth} in
               </Text>
-              <Text italic style={{ position: 'absolute', top: '80%', right: '70%', transform: 'translate(-50%, -50%)', fontWeight: 400 }}>
+              <Text italic>
                 {data.dimensions.width} in
               </Text>
             </Flex>
           </Flex>
-          <Flex vertical style={{ marginTop: 50 }}>
+          <Flex vertical className='info-codes-product'>
             <Title level={3}>Product codes in the physical shop</Title>
             <Text>If you wish, you can find this product in any of our branches, simply show at the counter any of the following codes.</Text>
-            <Flex style={{ marginTop: 40, justifyContent: 'space-between' }}>
+            <Flex>
               <Flex vertical>
                 <Title level={5}>QR Code</Title>
                 <img src={data.meta.qrCode} alt={`QR code for product ${data.title}`} />
@@ -148,15 +148,14 @@ const Product = ({ hasDiscount }) => {
                 <Barcode value={data.meta.barcode} />
               </Flex>
             </Flex>
-            <Text type='secondary' style={{ fontSize: '0.8rem' }}>Last update: {dayjs(data.meta.updatedAt).format('DD/MM/YYYY HH:mm a')}</Text>
+            <Text type='secondary'>Last update: {dayjs(data.meta.updatedAt).format('DD/MM/YYYY HH:mm a')}</Text>
           </Flex>
         </Flex>
-        <Flex style={{ width: '100%', maxWidth: '20%', display: 'inline-block', position: 'sticky', top: '10%', height: 'fit-content' }}>
-
-          <Flex vertical style={{ gap: 30 }}>
-            <Flex vertical style={{ position: 'relative' }}>
-              <Flex style={{ justifyContent: 'space-between', marginBottom: 10 }}>
-                <Flex style={{ alignItems: 'center' }}>
+        <Flex className='buy-product-container'>
+          <Flex vertical className='buy-product-price-container'>
+            <Flex vertical>
+              <Flex className='total-sold'>
+                <Flex>
                   <Text type='secondary'>New</Text>
                   <Divider type='vertical' style={{ borderColor: token.colorPrimaryBgHover }} />
                   <Text type='secondary'>{Number(Math.random() * 10000).toFixed(0)} sold</Text>
@@ -164,10 +163,10 @@ const Product = ({ hasDiscount }) => {
                 <IconThumbUpMotion />
               </Flex>
               {hasDiscount && <Text type='secondary' delete italic>${data.price}</Text>}
-              <Flex style={{ alignItems: 'center', width: '100%' }}>
-                <Title level={3} style={{ margin: 0, color: token.colorBgElevated, backgroundColor: token.colorPrimary, paddingBlock: '3%', paddingLeft: '5%', paddingRight: '13%' }} className='shadow-menu-subcategory'>${discountPrice}</Title>
+              <Flex className='buy-product-price'>
+                <Title level={3} className='shadow-menu-subcategory'>${discountPrice}</Title>
                 {hasDiscount && (
-                  <Text style={{ margin: 0, fontSize: '1.1rem', width: 'max-content', backgroundColor: token.colorBgElevated, paddingBlock: '2%', paddingInline: '5%', translate: '-20% 0' }} className='shadow-menu-subcategory'>{Math.floor(data.discountPercentage)}% OFF</Text>
+                  <Text className='shadow-menu-subcategory'>{Math.floor(data.discountPercentage)}% OFF</Text>
                 )}
               </Flex>
             </Flex>
@@ -177,8 +176,8 @@ const Product = ({ hasDiscount }) => {
             </Flex>
             <Text><span style={{ color: token.colorTextSecondary, fontWeight: 600 }}>Free shipping</span> nationwide</Text>
             <Text strong italic style={{ marginBlock: 10 }}>Stock available</Text>
-            <Flex vertical style={{ gap: 10 }}>
-              <Flex style={{ alignItems: 'center', gap: 10 }}>
+            <Flex vertical className='btns-payment-container'>
+              <Flex>
                 <Text>Cantidad:</Text>
                 <Select
                   variant='borderless'
@@ -196,73 +195,72 @@ const Product = ({ hasDiscount }) => {
             </Flex>
           </Flex>
           <Divider />
-          <Space direction='vertical'>
-            <Flex style={{ gap: 10 }}>
+          <Space direction='vertical' className='guarantees-product-container'>
+            <Flex>
               <IconReceiptRefund size={22} color={token.colorPrimary} />
               <Text type='secondary'>{data.returnPolicy}.</Text>
             </Flex>
-            <Flex style={{ gap: 10 }}>
+            <Flex>
               <IconCubeSend size={22} color={token.colorPrimary} />
               <Text type='secondary'>{data.shippingInformation}.</Text>
             </Flex>
-            <Flex style={{ gap: 10 }}>
+            <Flex>
               <IconShieldCheckFilled size={22} color={token.colorPrimary} />
               <Text type='secondary'>{data.warrantyInformation}.</Text>
             </Flex>
           </Space>
           <Divider />
-          <Flex vertical>
+          <Flex vertical className='payment-methods-product-container'>
             <Title level={4}>Payment methods</Title>
             <Flex vertical>
-              <Text style={{ fontWeight: 600 }}>Credit Card</Text>
-              <Flex style={{ flexWrap: 'wrap', gap: 10 }}>
-                <img src={amex} alt='American Express Icon' style={{ width: 45 }} />
-                <img src={hiper} alt='Hiper Icon' style={{ width: 45 }} />
-                <img src={mastercard} alt='MasterCard Icon' style={{ width: 45 }} />
-                <img src={visa} alt='Visa Icon' style={{ width: 45 }} />
+              <Text>Credit Card</Text>
+              <Flex>
+                <img src={amex} alt='American Express Icon' />
+                <img src={hiper} alt='Hiper Icon' />
+                <img src={mastercard} alt='MasterCard Icon' />
+                <img src={visa} alt='Visa Icon' />
               </Flex>
             </Flex>
             <Flex vertical>
-              <Text style={{ fontWeight: 600 }}>Debit Card</Text>
-              <Flex style={{ flexWrap: 'wrap', gap: 10 }}>
-                <img src={mastercard} alt='MasterCard Icon' style={{ width: 45 }} />
-                <img src={visa} alt='Visa Icon' style={{ width: 45 }} />
+              <Text>Debit Card</Text>
+              <Flex>
+                <img src={mastercard} alt='MasterCard Icon' />
+                <img src={visa} alt='Visa Icon' />
               </Flex>
             </Flex>
             <Flex vertical>
-              <Text style={{ fontWeight: 600 }}>Online Payment</Text>
-              <Flex style={{ flexWrap: 'wrap', gap: 10 }}>
-                <img src={paypal} alt='PayPal Icon' style={{ width: 45 }} />
+              <Text>Online Payment</Text>
+              <Flex>
+                <img src={paypal} alt='PayPal Icon' />
               </Flex>
             </Flex>
           </Flex>
-
         </Flex>
       </Flex>
       <Divider />
-      <Flex style={{ width: '100%', overflow: 'hidden', paddingInline: '5%' }} vertical>
-        <Title level={3} style={{ marginBottom: '3%', paddingLeft: '2%' }}>Related products</Title>
+      <Flex vertical className='related-products-container'>
+        <Title level={3}>Related products</Title>
         <CarouselProducts products={relatedData} typeCarousel='relatedProducts' />
       </Flex>
       <Divider />
-      <Flex vertical style={{ paddingInline: '5%', paddingBottom: '5%' }}>
+      <Flex vertical className='customer-reviews-container'>
 
         <Title level={2}>Customer reviews</Title>
-        <Flex style={{ position: 'relative', width: '100%', paddingInline: '5%' }}>
-          <Flex vertical style={{ backgroundColor: token.colorPrimary, color: token.colorWhite, padding: '2.5%', gap: 15, paddingRight: '5%', height: 'min-content' }} className='shadow-menu-subcategory'>
-            <Flex style={{ gap: 20, alignItems: 'center', color: token.colorWhite }}>
-              <Title level={1} style={{ margin: 0, color: 'inherit' }}>{data.rating}</Title>
-              <Flex vertical style={{ alignItems: 'center', gap: 5 }}>
-                <Rate defaultValue={data.rating} allowHalf disabled style={{ color: 'inherit', fontSize: '0.9rem' }} />
-                <Text style={{ fontSize: '0.8rem', color: 'inherit' }}>{data.reviews.length} reviews</Text>
+        <Flex>
+          <Flex vertical className='global-rating-container shadow-menu-subcategory'>
+            <Flex>
+              <Title level={1}>{data.rating}</Title>
+              <Flex vertical>
+                <Rate defaultValue={data.rating} allowHalf disabled />
+                <Text>{data.reviews.length} reviews</Text>
               </Flex>
             </Flex>
-            <Flex vertical style={{ gap: 5 }}>
+            <Flex vertical>
               {infoReviews && infoReviews.map((item, idx) => (
-                <Flex key={idx} style={{ alignItems: 'center', gap: 10 }}>
-                  <Flex style={{ alignItems: 'center', gap: 4 }}>
-                    <Text style={{ fontSize: '0.8rem', color: 'inherit' }}>{5 - idx}</Text>
-                    <IconStarFilled style={{ color: token.colorBgElevated }} size={9} />
+                <Flex key={idx}>
+                  <Flex>
+                    <Text>{5 - idx}</Text>
+                    <IconStarFilled size={9} />
                   </Flex>
                   <Progress
                     percent={item}
@@ -273,29 +271,29 @@ const Product = ({ hasDiscount }) => {
               ))}
             </Flex>
           </Flex>
-          <Flex vertical style={{ backgroundColor: token.colorBgElevated, paddingInline: '4%', paddingBlock: '2%', translate: '-5% 15%', maxWidth: '70%', width: '100%', gap: 20 }} className='shadow-menu-subcategory'>
+          <Flex vertical className='featured-reviews-container shadow-menu-subcategory'>
             <Title level={4}>Featured reviews</Title>
             {data.reviews.map((review, idx) => (
-              <Flex key={'review' + idx} vertical style={{ gap: 2 }}>
-                <Flex style={{ alignItems: 'center', justifyContent: 'space-between' }}>
-                  <Rate defaultValue={review.rating} allowHalf disabled style={{ fontSize: '0.8rem' }} />
-                  <Text style={{ fontSize: '0.8rem' }} type='secondary'>{dayjs(review.date).format('D MMMM[,] YYYY')}</Text>
+              <Flex key={'review' + idx} vertical>
+                <Flex className='review-header'>
+                  <Rate defaultValue={review.rating} allowHalf disabled />
+                  <Text type='secondary'>{dayjs(review.date).format('D MMMM[,] YYYY')}</Text>
                 </Flex>
-                <Flex style={{ alignItems: 'center', gap: 5 }}>
+                <Flex className='review-info-user'>
                   <Avatar style={{ backgroundColor: token.colorPrimary }} icon={<IconUserHexagon />} />
                   <Text>{review.reviewerName}</Text>
                   <Divider type='vertical' style={{ borderColor: token.colorPrimaryBg }} />
                   <Text>{review.reviewerEmail}</Text>
                 </Flex>
-                <Paragraph style={{ margin: 0, marginTop: '2%' }}>{review.comment}</Paragraph>
-                <Divider style={{ margin: 0, marginBlock: '1.5%', borderColor: token.colorPrimaryBg }} />
+                <Paragraph className='review-comment'>{review.comment}</Paragraph>
+                <Divider style={{ borderColor: token.colorPrimaryBg }} />
               </Flex>
             ))}
             <Button type='link'>Show all reviews...</Button>
           </Flex>
         </Flex>
       </Flex>
-      <Flex style={{ paddingInline: '5%' }}>
+      <Flex>
         <PromoBanner inCategory />
       </Flex>
     </Flex>
