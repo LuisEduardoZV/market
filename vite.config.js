6cf8b4b8e4ebd1913f04cf12ae6a29443c623d19
Luis Z <lezv.dc@gmail.com>
2024-09-23 15:53:54 -0600

    Correccion error pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/market/',
-  plugins: [react()],
-  define: {
-    'process.env.VITE_API_KEY_IMAGES': JSON.stringify(process.env.VITE_API_KEY_IMAGES)
-  }
+  plugins: [react()]
 })
